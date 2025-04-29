1bba3d14146ff3123f4c172ca61e7aeeccf703ed
Emilija Marija Dailydžionytė <emilija.dailydzionyte@mif.stud.vu.lt>
2025-04-30 01:16:17 +0300

    Updated SutartisF2Repo

--- Repositories/SutartisF2Repo.cs
+++ Repositories/SutartisF2Repo.cs
@@ -1,294 +1,132 @@
-﻿namespace Org.Ktu.Isk.P175B602.Autonuoma.Repositories;
+﻿namespace Org.Ktu.Isk.P175B602.FilmuNuoma.Repositories;
 
 using Newtonsoft.Json;
-
-using Org.Ktu.Isk.P175B602.Autonuoma.Models.SutartisF2;
-
+using Org.Ktu.Isk.P175B602.FilmuNuoma.Models;
+using System.Collections.Generic;
 
 /// <summary>
 /// Database operations related to 'Sutartis' entity.
 /// </summary>
 public class SutartisF2Repo : RepoBase
 {
-	public static List<SutartisL> ListSutartis()
-	{
-		var query =
-			$@"SELECT
-				s.nr,
-				s.sutarties_data as data,
-				CONCAT(d.vardas,' ', d.pavarde) as darbuotojas,
-				CONCAT(n.vardas,' ',n.pavarde) as nuomininkas,
-				b.name as busena
-			FROM
-				`{Config.TblPrefix}sutartys` s
-				LEFT JOIN `{Config.TblPrefix}darbuotojai` d ON s.fk_darbuotojas=d.tabelio_nr
-				LEFT JOIN `{Config.TblPrefix}klientai` n ON s.fk_klientas=n.asmens_kodas
-				LEFT JOIN `{Config.TblPrefix}sutarties_busenos` b ON s.busena=b.id
-			ORDER BY s.nr DESC";
+    public static List<Sutartis> ListSutartis()
+    {
+        var query = $@"SELECT * FROM `{Config.TblPrefix}sutartys` ORDER BY numeris ASC";
+        var drc = Sql.Query(query);
+        return Sql.MapAll<Sutartis>(drc, (dre, t) => new Sutartis
+        {
+            Numeris = dre.From<int>("numeris"),
+            PirkimoData = dre.From<DateTime>("pirkimo_data"),
+            Kaina = dre.From<decimal>("kaina"),
+            PristatymoArKitiMokesciai = dre.From<decimal>("pristatymo_ar_kiti_mokesciai"),
+            GrazinimoAdresas = dre.From<string>("grazinimo_adresas"),
+            PristatymoAdresas = dre.From<string>("pristatymo_adresas"),
+            PristatymoKaina = dre.From<decimal>("pristatymo_kaina"),
+            UzsakymoBusena = dre.From<string>("uzsakymo_busena")
+        });
+    }
 
-		var drc = Sql.Query(query);
+    public static Sutartis Find(int numeris)
+    {
+        var query = $@"SELECT * FROM `{Config.TblPrefix}sutartys` WHERE numeris = @numeris";
+        var drc = Sql.Query(query, cmd => cmd.Add("numeris", numeris));
+        return Sql.MapOne<Sutartis>(drc, (dre, t) => new Sutartis
+        {
+            Numeris = dre.From<int>("numeris"),
+            PirkimoData = dre.From<DateTime>("pirkimo_data"),
+            Kaina = dre.From<decimal>("kaina"),
+            PristatymoArKitiMokesciai = dre.From<decimal>("pristatymo_ar_kiti_mokesciai"),
+            GrazinimoAdresas = dre.From<string>("grazinimo_adresas"),
+            PristatymoAdresas = dre.From<string>("pristatymo_adresas"),
+            PristatymoKaina = dre.From<decimal>("pristatymo_kaina"),
+            UzsakymoBusena = dre.From<string>("uzsakymo_busena")
+        });
+    }
 
-		var result =
-			Sql.MapAll<SutartisL>(drc, (dre, t) => {
-				t.Nr = dre.From<int>("nr");
-				t.Darbuotojas = dre.From<string>("darbuotojas");
-				t.Nuomininkas = dre.From<string>("nuomininkas");
-				t.Data = dre.From<DateTime>("data");
-				t.Busena = dre.From<string>("busena");
-			});
+    public static int InsertSutartis(Sutartis sut)
+    {
+        var query = $@"INSERT INTO `{Config.TblPrefix}sutartys`
+                      (pirkimo_data, kaina, pristatymo_ar_kiti_mokesciai, grazinimo_adresas,
+                       pristatymo_adresas, pristatymo_kaina, uzsakymo_busena)
+                      VALUES (@pirkimo_data, @kaina, @pristatymo_ar_kiti_mokesciai, @grazinimo_adresas,
+                              @pristatymo_adresas, @pristatymo_kaina, @uzsakymo_busena)";
+        return (int)Sql.Insert(query, cmd =>
+        {
+            cmd.Add("pirkimo_data", sut.PirkimoData);
+            cmd.Add("kaina", sut.Kaina);
+            cmd.Add("pristatymo_ar_kiti_mokesciai", sut.PristatymoArKitiMokesciai);
+            cmd.Add("grazinimo_adresas", sut.GrazinimoAdresas);
+            cmd.Add("pristatymo_adresas", sut.PristatymoAdresas);
+            cmd.Add("pristatymo_kaina", sut.PristatymoKaina);
+            cmd.Add("uzsakymo_busena", sut.UzsakymoBusena);
+        });
+    }
 
-		return result;
-	}
+    public static void UpdateSutartis(Sutartis sut)
+    {
+        var query = $@"UPDATE `{Config.TblPrefix}sutartys`
+                      SET pirkimo_data = @pirkimo_data,
+                          kaina = @kaina,
+                          pristatymo_ar_kiti_mokesciai = @pristatymo_ar_kiti_mokesciai,
+                          grazinimo_adresas = @grazinimo_adresas,
+                          pristatymo_adresas = @pristatymo_adresas,
+                          pristatymo_kaina = @pristatymo_kaina,
+                          uzsakymo_busena = @uzsakymo_busena
+                      WHERE numeris = @numeris";
+        Sql.Update(query, cmd =>
+        {
+            cmd.Add("pirkimo_data", sut.PirkimoData);
+            cmd.Add("kaina", sut.Kaina);
+            cmd.Add("pristatymo_ar_kiti_mokesciai", sut.PristatymoArKitiMokesciai);
+            cmd.Add("grazinimo_adresas", sut.GrazinimoAdresas);
+            cmd.Add("pristatymo_adresas", sut.PristatymoAdresas);
+            cmd.Add("pristatymo_kaina", sut.PristatymoKaina);
+            cmd.Add("uzsakymo_busena", sut.UzsakymoBusena);
+            cmd.Add("numeris", sut.Numeris);
+        });
+    }
 
-	public static SutartisCE FindSutartisCE(int nr)
-	{
-		var query = $@"SELECT * FROM `{Config.TblPrefix}sutartys` WHERE nr=?nr";
-		var drc =
-			Sql.Query(query, args => {
-				args.Add("?nr", nr);
-			});
+    public static void DeleteSutartis(int nr)
+    {
+        var query = $@"DELETE FROM `{Config.TblPrefix}sutartys` where numeris = @nr";
+        Sql.Delete(query, cmd => cmd.Add("nr", nr));
+    }
 
-		var result =
-			Sql.MapOne<SutartisCE>(drc, (dre, t) => {
-				//make a shortcut
-				var sut = t.Sutartis;
+    public static List<UzsakytaPreke> ListUzsakytaPaslauga(int sutartisId)
+    {
+        var query = $@"SELECT up.* FROM `{Config.TblPrefix}uzsakytos_prekes` up
+                      WHERE up.fk_sutartis = @sutartisId
+                      ORDER BY up.fk_preke ASC";
 
-				//
-				sut.Nr = dre.From<int>("nr");
-				sut.SutartiesData = dre.From<DateTime>("sutarties_data");
-				sut.NuomosDataLaikas = dre.From<DateTime>("nuomos_data_laikas");
-				sut.PlanuojamaGrDataLaikas = dre.From<DateTime>("planuojama_grazinimo_data_laikas");
-				sut.FaktineGrDataLaikas = dre.From<DateTime?>("faktine_grazinimo_data_laikas");
-				sut.PradineRida = dre.From<int>("pradine_rida");
-				sut.GalineRida = dre.From<int?>("galine_rida");
-				sut.Kaina = dre.From<decimal>("kaina");
-				sut.DegaluKiekisPaimant = dre.From<int>("degalu_kiekis_paimant");
-				sut.DegaluKiekisGrazinant = dre.From<int?>("dagalu_kiekis_grazinus");
-				sut.FkBusena = dre.From<int>("busena");
-				sut.FkKlientas = dre.From<string>("fk_klientas");
-				sut.FkDarbuotojas = dre.From<string>("fk_darbuotojas");
-				sut.FkAutomobilis = dre.From<int>("fk_automobilis");
-				sut.FkGrazinimoVieta = dre.From<int>("fk_grazinimo_vieta");
-				sut.FkPaemimoVieta = dre.From<int>("fk_paemimo_vieta");
-			});
+        var drc = Sql.Query(query, cmd => cmd.Add("sutartisId", sutartisId));
 
-		return result;
-	}
+        return Sql.MapAll<UzsakytaPreke>(drc, (dre, t) => new UzsakytaPreke
+        {
+            Id = dre.From<int>("id"),
+            Kiekis = dre.From<int>("kiekis"),
+            Kaina = dre.From<decimal>("kaina"),
+            PrekeId = dre.From<int>("fk_preke"),
+            SutartisNumeris = dre.From<int>("fk_sutartis")
+        });
+    }
 
-	public static int InsertSutartis(SutartisCE sutCE)
-	{
-		var query =
-			$@"INSERT INTO `{Config.TblPrefix}sutartys`
-			(
-				`sutarties_data`,
-				`nuomos_data_laikas`,
-				`planuojama_grazinimo_data_laikas`,
-				`faktine_grazinimo_data_laikas`,
-				`pradine_rida`,
-				`galine_rida`,
-				`kaina`,
-				`degalu_kiekis_paimant`,
-				`dagalu_kiekis_grazinus`,
-				`busena`,
-				`fk_klientas`,
-				`fk_darbuotojas`,
-				`fk_automobilis`,
-				`fk_grazinimo_vieta`,
-				`fk_paemimo_vieta`
-			)
-			VALUES(
-				?sutdata,
-				?nuomdata,
-				?plgrlaikas,
-				?fkgrlaikas,
-				?prrida,
-				?glrida,
-				?kaina,
-				?dkiekispa,
-				?dkiekisgr,
-				?busena,
-				?klientas,
-				?darbuotojas,
-				?automobilis,
-				?grvieta,
-				?pavieta
-			)";
+    public static void InsertUzsakytaPaslauga(int sutartisId, UzsakytaPreke up)
+    {
+        var query = $@"INSERT INTO `{Config.TblPrefix}uzsakytos_prekes`
+                      (fk_sutartis, fk_preke, kiekis, kaina)
+                      VALUES (@fk_sutartis, @fk_preke, @kiekis, @kaina)";
+        Sql.Insert(query, cmd =>
+        {
+            cmd.Add("fk_sutartis", sutartisId);
+            cmd.Add("fk_preke", up.PrekeId);
+            cmd.Add("kiekis", up.Kiekis);
+            cmd.Add("kaina", up.Kaina);
+        });
+    }
 
-		var nr =
-			Sql.Insert(query, args => {
-				//make a shortcut
-				var sut = sutCE.Sutartis;
-
-				//
-				args.Add("?sutdata", sut.SutartiesData);
-				args.Add("?nuomdata", sut.NuomosDataLaikas);
-				args.Add("?plgrlaikas", sut.PlanuojamaGrDataLaikas);
-				args.Add("?fkgrlaikas", sut.FaktineGrDataLaikas);
-				args.Add("?prrida", sut.PradineRida);
-				args.Add("?glrida", sut.GalineRida);
-				args.Add("?kaina", sut.Kaina);
-				args.Add("?dkiekispa", sut.DegaluKiekisPaimant);
-				args.Add("?dkiekisgr", sut.DegaluKiekisGrazinant);
-				args.Add("?busena", sut.FkBusena);
-				args.Add("?darbuotojas", sut.FkDarbuotojas);
-				args.Add("?klientas", sut.FkKlientas);
-				args.Add("?automobilis", sut.FkAutomobilis);
-				args.Add("?grvieta", sut.FkGrazinimoVieta);
-				args.Add("?pavieta", sut.FkPaemimoVieta);
-			});
-
-		return (int)nr;
-	}
-
-	public static void UpdateSutartis(SutartisCE sutCE)
-	{
-		var query =
-			$@"UPDATE `{Config.TblPrefix}sutartys`
-			SET
-				`sutarties_data` = ?sutdata,
-				`nuomos_data_laikas` = ?nuomdata,
-				`planuojama_grazinimo_data_laikas` = ?plgrlaikas,
-				`faktine_grazinimo_data_laikas` = ?fkgrlaikas,
-				`pradine_rida` = ?prrida,
-				`galine_rida` = ?glrida,
-				`kaina` = ?kaina,
-				`degalu_kiekis_paimant` = ?dkiekispa,
-				`dagalu_kiekis_grazinus` = ?dkiekisgr,
-				`busena` = ?busena,
-				`fk_klientas` = ?klientas,
-				`fk_darbuotojas` = ?darbuotojas,
-				`fk_automobilis` = ?automobilis,
-				`fk_grazinimo_vieta` = ?grvieta,
-				`fk_paemimo_vieta` = ?pavieta
-			WHERE nr=?nr";
-
-		Sql.Update(query, args => {
-			//make a shortcut
-			var sut = sutCE.Sutartis;
-
-			//
-			args.Add("?sutdata", sut.SutartiesData);
-			args.Add("?nuomdata", sut.NuomosDataLaikas);
-			args.Add("?plgrlaikas", sut.PlanuojamaGrDataLaikas);
-			args.Add("?fkgrlaikas", sut.FaktineGrDataLaikas);
-			args.Add("?prrida", sut.PradineRida);
-			args.Add("?glrida", sut.GalineRida);
-			args.Add("?kaina", sut.Kaina);
-			args.Add("?dkiekispa", sut.DegaluKiekisPaimant);
-			args.Add("?dkiekisgr", sut.DegaluKiekisGrazinant);
-			args.Add("?busena", sut.FkBusena);
-			args.Add("?darbuotojas", sut.FkDarbuotojas);
-			args.Add("?klientas", sut.FkKlientas);
-			args.Add("?automobilis", sut.FkAutomobilis);
-			args.Add("?grvieta", sut.FkGrazinimoVieta);
-			args.Add("?pavieta", sut.FkPaemimoVieta);
-
-			args.Add("?nr", sut.Nr);
-		});
-	}
-
-	public static void DeleteSutartis(int nr)
-	{
-		var query = $@"DELETE FROM `{Config.TblPrefix}sutartys` where nr=?nr";
-		Sql.Delete(query, args => {
-			args.Add("?nr", nr);
-		});
-	}
-
-	public static List<SutartiesBusena> ListSutartiesBusena()
-	{
-		var query = $@"SELECT * FROM `{Config.TblPrefix}sutarties_busenos` ORDER BY id ASC";
-		var drc = Sql.Query(query);
-
-		var result =
-			Sql.MapAll<SutartiesBusena>(drc, (dre, t) => {
-				t.Id = dre.From<int>("id");
-				t.Name = dre.From<string>("name");
-			});
-
-		return result;
-	}
-
-	public static List<SutartisCE.UzsakytaPaslaugaM> ListUzsakytaPaslauga(int sutartisId)
-	{
-		var query =
-			$@"SELECT *
-			FROM `{Config.TblPrefix}uzsakytos_paslaugos`
-			WHERE fk_sutartis = ?sutartisId
-			ORDER BY fk_paslauga ASC, fk_kaina_galioja_nuo ASC";
-
-		var drc =
-			Sql.Query(query, args => {
-				args.Add("?sutartisId", sutartisId);
-			});
-
-		var result =
-			Sql.MapAll<SutartisCE.UzsakytaPaslaugaM>(drc, (dre, t) => {
-				t.Paslauga =
-					//we use JSON here to make serialization/deserializaton of composite key more convenient
-					JsonConvert.SerializeObject(new {
-						FkPaslauga = dre.From<int>("fk_paslauga"),
-						GaliojaNuo = dre.From<DateTime>("fk_kaina_galioja_nuo")
-					});
-				t.Kiekis = dre.From<int>("kiekis");
-				t.Kaina = dre.From<decimal>("kaina");
-			});
-
-		for( int i = 0; i < result.Count; i++ )
-			result[i].InListId = i;
-
-		return result;
-	}
-
-	public static void InsertUzsakytaPaslauga(int sutartisId, SutartisCE.UzsakytaPaslaugaM up)
-	{
-		//deserialize 'Paslauga' foreign keys from 'UzsakytaPaslauga' view model key
-		var fks =
-			JsonConvert.DeserializeAnonymousType(
-				up.Paslauga,
-				//this creates object of correct shape that is filled in by the JSON deserializer
-				new {
-					FkPaslauga = 1,
-					GaliojaNuo = DateTime.Now
-				}
-			);
-
-		//
-		var query =
-			$@"INSERT INTO `{Config.TblPrefix}uzsakytos_paslaugos`
-				(
-					fk_sutartis,
-					fk_kaina_galioja_nuo,
-					fk_paslauga,
-					kiekis,
-					kaina
-				)
-				VALUES(
-					?fk_sutartis,
-					?galioja_nuo,
-					?fk_paslauga,
-					?kiekis,
-					?kaina
-				)";
-
-		Sql.Insert(query, args => {
-			args.Add("?fk_sutartis", sutartisId);
-			args.Add("?galioja_nuo", fks.GaliojaNuo);
-			args.Add("?fk_paslauga", fks.FkPaslauga);
-			args.Add("?kaina", up.Kaina);
-			args.Add("?kiekis", up.Kiekis);
-		});
-	}
-
-	public static void DeleteUzsakytaPaslaugaForSutartis(int sutartis)
-	{
-		var query =
-			$@"DELETE FROM a
-			USING `{Config.TblPrefix}uzsakytos_paslaugos` as a
-			WHERE a.fk_sutartis=?fkid";
-
-		Sql.Delete(query, args => {
-			args.Add("?fkid", sutartis);
-		});
-	}
+    public static void DeleteUzsakytaPaslaugaForSutartis(int sutartisId)
+    {
+        var query = $@"DELETE FROM `{Config.TblPrefix}uzsakytos_prekes` WHERE fk_sutartis = @fk_sutartis";
+        Sql.Delete(query, cmd => cmd.Add("fk_sutartis", sutartisId));
+    }
 }
